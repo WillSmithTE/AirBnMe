6434a5bbb9788347e756a9632dc40d540995412a
Will Smith <willsmithte@gmail.com>
2019-07-31 22:51:12 +1000

    fix github link

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -9,7 +9,7 @@ export class About extends React.Component {
             <a style={{ display: 'inline;' }} href='https://twitter.com/dontplaynojames/status/1020568969965473793' target='_blank' rel="noopener noreferrer" > tweet </a>
              of Geoffrey James where anyone can post their accommodation and I can book it. Only me :)</p>
             <TextLink url='https://www.linkedin.com/in/willsmithte/' text='LinkedIn' />
-            <TextLink url='github.com/WillSmithTE/' text='GitHub' />
+            <TextLink url='https://github.com/WillSmithTE/' text='GitHub' />
         </div>;
     }
 }
