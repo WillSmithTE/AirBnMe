012a8ec2dee79202a11ed3b820e6bbf28bb3e69d
Will Smith <willsmithte@gmail.com>
2019-08-09 05:43:33 +1000

    try adding build to git

---
+++ build/precache-manifest.be06e7c651c7e153f91d289487d19b99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9754ec951e731199f45101a9f802d5f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673bd31bc1b49ee4b951",
+    "url": "/static/css/2.6b000025.chunk.css"
+  },
+  {
+    "revision": "9811ec841798a1dfcf30",
+    "url": "/static/css/main.e05755d1.chunk.css"
+  },
+  {
+    "revision": "673bd31bc1b49ee4b951",
+    "url": "/static/js/2.74eab4f5.chunk.js"
+  },
+  {
+    "revision": "9811ec841798a1dfcf30",
+    "url": "/static/js/main.f3c2c343.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8338261a486642c9977de87f5aed2d5e",
+    "url": "/static/media/logo.8338261a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react-logo.5d5d9eef.svg"
+  }
+]);
